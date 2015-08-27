55341d08efc6ac4030a9102c1c2ce506b12886aa
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-08-27 11:55:38 +0100

    Switch to counting seconds since the beginning of the week.

--- app/models/event.js
+++ app/models/event.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   temp: DS.attr('number'),
-  day:  DS.attr('number'),
-  time: DS.attr('number'),
+
+  // Seconds since 00:00 Monday.
+  seconds: DS.attr('number'),
 });
