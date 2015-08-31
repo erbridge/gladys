64f2ef7576b9a8ed7f848d0cbd2529111200a338
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-08-31 14:56:50 +0100

    Add the events draggable to change the time.

--- app/components/event-edit.js
+++ app/components/event-edit.js
@@ -3,15 +3,29 @@ import Ember from 'ember';
 const secondsInDay = 86400;
 
 export default Ember.Component.extend({
-  classNames: [ 'event' ],
+  classNames:        [ 'event' ],
   attributeBindings: [ 'style' ],
 
   style: Ember.computed('event.secondsToday', function() {
     const seconds = this.get('event').get('secondsToday');
     const top     = 100 * seconds / secondsInDay;
 
-    return `top: ${top}%`;
+    return new Ember.Handlebars.SafeString(`top: ${top}%;`);
   }),
+
+  makeDraggable: Ember.on('didInsertElement', function() {
+    Ember.$(this.element).draggable({
+      axis:        'y',
+      containment: 'parent',
+      drag:        this.updateTime.bind(this),
+    });
+  }),
+
+  updateTime(ev, ui) {
+    const dayProportion = ui.position.top / ui.helper.parent().height();
+
+    this.get('event').set('secondsToday', Math.round(dayProportion * secondsInDay));
+  },
 
   actions: {
     remove() {
