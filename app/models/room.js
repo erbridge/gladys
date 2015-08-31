abf0570518be66079cfea8018b1d4b02cdf205c7
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-08-31 14:24:18 +0100

    Make the room label readonly.

--- app/models/room.js
+++ app/models/room.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  label:          DS.attr('string'),
+  label:          DS.attr('string', { readOnly: true }),
   activeSchedule: DS.belongsTo('schedule', { async: true }),
   devices:        DS.hasMany('device', { async: true }),
 });
