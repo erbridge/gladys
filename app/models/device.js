e90a4ae23eefc032e5334c217e0c10ccfe7bfc27
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-09-01 15:28:46 +0100

    Display the current room temperature.

--- app/models/device.js
+++ app/models/device.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  label:    DS.attr('string'),
+  label:    DS.attr('string', { readOnly: true }),
   type:     DS.attr('string'),
   state:    DS.attr('string'),
   temp:     DS.attr('number'),
--- app/models/room.js
+++ app/models/room.js
@@ -1,7 +1,28 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   label:          DS.attr('string', { readOnly: true }),
   activeSchedule: DS.belongsTo('schedule', { async: true }),
   devices:        DS.hasMany('device', { async: true }),
+
+  relays: Ember.computed.filter('devices.@each.type', function(device) {
+    return device.get('type') === 'relay';
+  }),
+
+  thermostats: Ember.computed.filter('devices.@each.type', function(device) {
+    return device.get('type') === 'thermostat';
+  }),
+
+  temp: Ember.computed('thermostats.@each.label', 'thermostats.@each.temp', function() {
+    let temp;
+
+    this.get('devices').forEach(function(device) {
+      if (device.get('label').indexOf('Thermostat') !== -1) {
+        temp = device.get('temp');
+      }
+    });
+
+    return temp;
+  }),
 });
