27ce699ac1f011eb4cb44bb80ab6328af6fa7ce6
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-10-15 00:21:18 +0100

    Retry failed ajax requests.

--- app/utils/request.js
+++ app/utils/request.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 const remoteUrl = 'http://192.168.1.31/cgi-bin/cmh/gladys_relay.sh/';
 
 export default {
-  send(data, dataType) {
+  send(data, dataType, retryCount=5) {
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      var config = {
+      const config = {
         type: 'GET',
         url:  remoteUrl,
         data: data,
@@ -18,17 +18,33 @@ export default {
       console.log(data);
       console.log(config);
 
-      Ember.$.ajax(config).then(function(data) {
-        console.log(data);
+      const attemptSend = function() {
+        Ember.$.ajax(config).then(function(data) {
+          console.log(data);
 
-        Ember.run(null, resolve, data);
-      }, function(jqXHR) {
-        console.log('error');
-        console.log(jqXHR);
+          Ember.run(null, resolve, data);
+        }, function(jqXHR) {
+          console.log('error');
+          console.log(jqXHR);
 
-        jqXHR.then = null;
-        Ember.run(null, reject, jqXHR);
-      });
+          if (retryCount > 0) {
+            console.log('retrying');
+
+            retryCount--;
+
+            setTimeout(function() {
+              attemptSend();
+            }, 500);
+          } else {
+            console.log('rejecting');
+
+            jqXHR.then = null;
+            Ember.run(null, reject, jqXHR);
+          }
+        });
+      };
+
+      attemptSend();
     });
   }
 };
